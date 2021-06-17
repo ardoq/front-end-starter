e3013a47ae1ac55d2e5712eb2c06c5b9eef39298
andreajl <andreasleppis@gmail.com>
2021-06-17 08:18:18 +0200

    Upgrade deps

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,14 @@ import styled from 'styled-components';
 interface AppProps {
   name: string;
 }
-export const Container = styled.div`
+
+const Container = styled.div`
   margin-top: 20px;
   text-align: center;
 `;
 
-export default function App({ name }: AppProps) {
+const App = ({ name }: AppProps) => {
   return <Container>Hello {name}!</Container>;
-}
+};
+
+export default App;
